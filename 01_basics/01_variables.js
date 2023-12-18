91b7d9fdb7ad66c1ea16a03afff9d52bfdd72e47
Shivdutt-Pachori <154082077+Shivdutt-Pachori@users.noreply.github.com>
2023-12-18 05:47:18 +0000

    learn variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "mymail@google.com"
+var accountPassword = "12345"//not preffered to use has issue in scope
+accountCity = "Jaipur"
+
+accountEmail = "nobita@shizuka.com"
+accountPassword = "54321"
+accountCity = "Bengalore"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])

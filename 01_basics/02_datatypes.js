6ad3c695425c935cc03f48ea15e616372b1398c1
Shivdutt-Pachori <154082077+Shivdutt-Pachori@users.noreply.github.com>
2023-12-18 06:15:52 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3) we are using Node JS not browser
+let name = "doremon"
+let age = 3
+let isLoggedin = true
+
+//number
+//bigint
+//string
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined

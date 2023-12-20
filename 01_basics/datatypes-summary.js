ed503957420093750eb6d2a4ec12a30193584bbd
Shivdutt-Pachori <154082077+Shivdutt-Pachori@users.noreply.github.com>
2023-12-20 05:27:00 +0000

    datypes summary primitive non- primitive

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,41 @@
+// primitive - pass by value
+// non - primitive -> pass by reference eg:- arrays,function
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+//console.log(id)
+console.log(id === anotherId)
+
+//Array
+const heros = ["shaktiman", "nagraj", "doga"];
+
+//Object
+{
+    name: "shaktiman";
+    age: 34;
+}
+//another way
+let myObj = {
+    name: "shaktiman",
+    age: 34,
+}
+
+//function
+const myFunction = function(){
+    console.log("Hello world");
+}
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+

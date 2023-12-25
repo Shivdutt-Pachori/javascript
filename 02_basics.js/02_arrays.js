76955495acc3504c64559cb1a41caaffd9deb91f
Shivdutt-Pachori <154082077+Shivdutt-Pachori@users.noreply.github.com>
2023-12-25 18:03:52 +0000

    Array completed

--- 02_basics.js/02_arrays.js
+++ 02_basics.js/02_arrays.js
@@ -15,3 +15,16 @@ const allHeroes = marvelHeroes.concat(dcHeroes);// concat returns an array by co
 
 const allNewHeroes = [...marvelHeroes, ...dcHeroes];
 console.log(allNewHeroes);
+
+const anotherArray = [1,2,3,[4,5,6],7,[6,7,[4,5]]];
+const realAnotherArray = anotherArray.flat(Infinity);
+console.log(realAnotherArray);
+
+console.log(Array.isArray("Spiderman"));
+console.log(Array.from("Spiderman"));
+console.log(Array.from({name: "Spiderman"}));//interesting (we need to specify that we want an array of keys or values if we do not specify anything by default it will return empty array)
+
+const score1 = 100;
+const score2 = 200;
+const score3 = 300;
+console.log(Array.of(score1,score2,score3));

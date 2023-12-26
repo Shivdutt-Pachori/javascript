7e5b54360aae77877e7688900af1afdab568e750
Shivdutt-Pachori <154082077+Shivdutt-Pachori@users.noreply.github.com>
2023-12-26 05:58:48 +0000

    object part 2

---
+++ 02_basics.js/04_objects.js
@@ -0,0 +1,56 @@
+//const tinderuser = new Object(); // singleton object
+const tinderUser = {// non - singleton object
+    id: "123abc",
+    name: "Spiderman",
+    isLoggedIn: false
+} 
+
+// tinderUser.id = "123abc"; // another way to add values to object
+// tinderUser.name = "Shaktiman";
+// tinderUser.isLoggedIn = false;
+
+//console.log(tinderUser);
+
+
+const regularUser = {
+    email: "some@gamil.com",
+    fullName: {
+        userFullName: {
+            firstName: "spider",
+            lastname: "man"
+        }
+    }
+}
+
+console.log(regularUser);
+console.log(regularUser.fullName.userFullName.firstName);
+
+const obj1 = { 1: "a", 2: "b"};
+const obj2 = { 3: "a", 4: "b"};
+
+//const obj3 = Object.assign({}, obj1, obj2);
+
+const obj3 = {...obj1,...obj2};
+console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        name: "fisrt"
+    },
+    {
+        id: 2,
+        name: "second"
+    },
+    {
+        id: 3,
+        name: "third"
+    }
+]
+
+console.log(users[0].name);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));

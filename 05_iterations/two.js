9d065c7915ebf6f94d7831567ad0114bd829a63c
Shivdutt-Pachori <154082077+Shivdutt-Pachori@users.noreply.github.com>
2024-01-05 17:51:43 +0000

    while do_while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,16 @@
+let myArray = ["flash", "batman", "superman"];
+let arr = 0;
+while(arr < myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr++;
+}
+
+let score = 11;
+// do {
+//     console.log(`score is ${score}`);
+//     score++;
+// } while (score <= 10);
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score <= 10);

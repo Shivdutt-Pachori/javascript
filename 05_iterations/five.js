03a1335244eb428bc10bd888dc8218e721317a4e
Shivdutt-Pachori <154082077+Shivdutt-Pachori@users.noreply.github.com>
2024-01-06 05:28:50 +0000

    how to deal with array of objects

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,1 +1,38 @@
-const coding = ["js", "ruby", "java", "python"]
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (val) {
+//     console.log(val);
+// })
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe);
+
+coding.forEach((item,index,arr) => {
+   // console.log(arr,index,item);
+})
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFIleName: "js"
+    },
+    {
+        languageName: "cpp",
+        languageFIleName: "cpp"
+    },
+    {
+        languageName: "python",
+        languageFIleName: "py"
+    },
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+})
+
